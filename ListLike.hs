1a1b5785d7e70603de15894ad26f33573ead72ee
John Goerzen <jgoerzen@complete.org>
2007-09-08 13:16:34 -0500

    Working on modules

--- ListLike.hs
+++ ListLike.hs
@@ -22,32 +22,61 @@ Written by John Goerzen, jgoerzen\@complete.org
 -}
 
 module ListLike (-- * Introduction
+                 -- $intro
                  -- * Creation & Basic Functions
+                 empty, singleton, 
+                 cons, snoc, append, head, last, tail, init, null, length,
+                 -- ** Conversions
+                 toList, fromList, fromListLike
                  -- * Reducing lists (folds)
+                 FoldableLL(foldl, foldl', foldl1, foldr, foldr', foldr1),
                  -- ** Special folds
+                 concat, concatMap, 
+                 and, or,
+                 any, all,
+                 sum, product,
+                 maximum, minimum,
                  -- * Building lists
                  -- ** Scans
                  -- ** Accumulating maps
                  -- ** Infinite lists
+                 iterate, repeat, replicate, cycle,
                  -- ** Unfolding
                  -- * Sublists
                  -- ** Extracting sublists
+                 take, drop, splitAt, takeWhile, dropWhile, span, break,
+                 group, inits, tails, 
                  -- ** Predicates
+                 isPrefixOf, isSuffixOf, isInfixOf,
                  -- * Searching lists
                  -- ** Searching by equality
+                 elem, notElem,
                  -- ** Searching with a predicate
+                 find, filter, partition,
                  -- * Indexing lists
+                 index, elemIndex, elemIndices, findIndex, findIndices,
                  -- * Zipping and unzipping lists
+                 zip, zipWith, unzip,
                  -- * Special lists
                  -- ** \"Set\" operations
+                 nub, delete, deleteFirsts, union, intersect,
                  -- ** Ordered lists
+                 sort, insert,
                  -- * Generalized functions
                  -- ** The \"By\" operations
                  -- *** User-supplied equality (replacing an Eq context)
+                 nubBy, deleteBy, deleteFirstsBy, unionBy, intersectBy,
+                 groupBy,
                  -- *** User-supplied comparison (replacing an Ord context)
+                 sortBy, insertBy, maximumBy, minimumBy,
                  -- ** The \"generic\" operations
+                 genericLength, genericTake, genericDrop, genericSplitAt,
+                 -- genericIndex,
+                 genericReplicate,
                  -- * The ListLike class
-                 ListLike(..)
+                 ListLike(..),
+                 -- * The InfiniteListLike class
+                 InfiniteListLike(..),
                 )
        where
 import Prelude hiding (length, head, last, null, tail, map, filter, concat, 
@@ -319,7 +348,10 @@ class (F.FoldableLL full item, Monoid full) =>
     elemIndex :: Eq item => item -> full -> Maybe Int
     elemIndex e l = findIndex (== e) l
 
-
+    {- | Returns the indices of the matching elements.  See also 
+       'findIndices' -}
+    elemIndices :: (Eq item, ListLike result Int) => item -> full -> result
+    elemIndices i l = findIndices (== i) l
 
     {- | Take a function and return the index of the first matching element,
          or Nothing if no element matches -}
@@ -551,4 +583,34 @@ instance ListLike BS.ByteString Word8 where
     head = BS.head
     tail = BS.tail
     rigidMap = BS.map
+
+{- $intro
+Welcome to ListLike.
+
+This module provides abstractions over typical list operations.
+It is designed to let you freely interchange different ways to represent
+sequences of data.  It works with lists, various types of ByteStrings,
+and much more.
+
+In this module, you'll find generic versions of most of the functions
+you're used to using in the "Prelude" or from "Data.List".  They carry the
+same names, too.  Therefore, you'll want to be careful how you import
+the module.  I suggest using:
+
+>import qualified ListLike as LL
+
+Then, you can use LL.fold, LL.map, etc. to get the generic version of
+the functions you want.  Alternatively, you can hide the other versions
+from Prelude and import specific generic functions from here, such as:
+
+>import Prelude hiding (map)
+>import ListLike (map)
+
+In most cases, functions here can act as drop-in replacements for their
+list-specific counterparts.  They will use the same underlying implementations
+for lists, so there should be no performance difference.
+    
+You can make your own types instances of 'ListLike' as well.  For more
+details, see the notes for the 'ListLike' typeclass.
+-}
 
