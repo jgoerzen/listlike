9f57534f8dc1dcda445a545e4b4dda33ccf16de5
John Goerzen <jgoerzen@complete.org>
2007-09-11 07:28:30 -0500

    Added MyList

--- testsrc/runtests.hs
+++ testsrc/runtests.hs
@@ -30,11 +30,36 @@ import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ListLike as LL
 import qualified Data.Map as Map
 import qualified Data.Array as A
+import qualified Data.Foldable as F
 import System.Random
 import Test.HUnit
 import Text.Printf
 import Data.Word
 import Data.List
+import Data.Monoid
+
+data MyList a = MyList [a]
+
+instance (Show a) => Show (MyList a) where
+    show (MyList x) = "MyList " ++ show x
+instance (Eq a) => Eq (MyList a) where
+    (==) (MyList x) (MyList y) = x == y
+
+instance LL.FoldableLL (MyList a) a where
+    foldr f i (MyList x) = foldr f i x
+    foldl f i (MyList x) = foldl f i x
+    foldr1 f (MyList x) = foldr1 f x
+    foldl1 f (MyList x) = foldl1 f x
+
+instance Monoid (MyList a) where
+    mempty = MyList []
+    mappend (MyList x) (MyList y) = MyList (x ++ y)
+
+instance LL.ListLike (MyList a) a where
+    singleton x = MyList [x]
+    head (MyList x) = head x
+    tail (MyList x) = MyList (tail x)
+    null (MyList x) = null x
 
 class (Arbitrary b, Show b, LL.ListLike a b, Eq b) => TestLL a b where
     tl :: a -> [b]
@@ -45,6 +70,10 @@ class (Arbitrary b, Show b, LL.ListLike a b, Eq b) => TestLL a b where
         tl (nativefunc (fl listdata)) == listfunc listdata
 
 instance (Arbitrary a, Show a, Eq a) => TestLL [a] a where
+    tl = LL.toList
+    fl = LL.fromList
+
+instance (Arbitrary a, Show a, Eq a) => TestLL (MyList a) a where
     tl = LL.toList
     fl = LL.fromList
 
@@ -107,6 +136,13 @@ instance Arbitrary (A.Array Int Int) where
                      return $ A.listArray (0, n - 1) arblist
     coarbitrary a = coarbitrary (A.elems a)
      
+instance Arbitrary (MyList Int) where
+    arbitrary = sized (\n -> choose (0, n) >>= myVector)
+        where myVector n =
+                  do arblist <- vector n
+                     return (MyList arblist)
+    coarbitrary (MyList x) = coarbitrary x
+
 instance Random Word8 where
     randomR (a, b) g = (\(x, y) -> (fromInteger x, y)) $
                        randomR (toInteger a, toInteger b) g
@@ -123,6 +159,7 @@ ta ::
 ta msg nativetest listtest = 
     TestList 
     [t (msg ++ " [Int]") (cl (nativetest::([Int] -> [Int])) listtest),
+     t (msg ++ " MyList Int") (cl (nativetest::(MyList Int -> MyList Int)) listtest),
      t (msg ++ " Map") (cl (nativetest::(Map.Map Int Int -> Map.Map Int Int)) listtest),
      t (msg ++ " ByteString") (cl (nativetest::(BS.ByteString -> BS.ByteString)) listtest),
      t (msg ++ " ByteString.Lazy") (cl (nativetest::(BSL.ByteString -> BSL.ByteString)) listtest),
