c52f478756713a6595cb4702f2f37490979eb040
John Goerzen <jgoerzen@complete.org>
2007-09-11 18:59:54 -0500

    More tweaking

--- testsrc/runtests.hs
+++ testsrc/runtests.hs
@@ -27,18 +27,18 @@ import TestInfrastructure
 -- prop_singleton :: (Eq i,LL.ListLike f i) => f -> i -> Bool
 prop_singleton f x = (LL.toList $ asTypeOf (LL.singleton x) f) == [x]
 
+
+
 prop_empty f x = (LL.toList l == []) && (LL.null l) && (LL.length l == 0)
     where l = asTypeOf LL.empty f
 
-prop_tofromlist f x = 
+prop_tofromlist f = 
     LL.toList f == l && 
     LL.length f == length l &&
     f == (LL.fromList . LL.toList $ f)
-    where l = asTypeOf (LL.toList f) [x]
+    where l = LL.toList f
 
-prop_length f x =
-    LL.length f == length l
-    where l = asTypeOf (LL.toList f) [x]
+prop_length f = LL.length f == length (LL.toList f)
 
 -- | all props, 2 args: full and item
 apfi :: String -> (forall f i. (Eq i, Eq f, LL.ListLike f i) => (f -> i -> Bool)) -> Test
@@ -57,15 +57,15 @@ apfi msg x = TestList $
      t (msg ++ " Array Int Bool") (x::A.Array Int Bool -> Bool -> Bool)
     ]
 
-{-
 -- | all props, 1 arg: full
-apf :: String -> (forall f i. (Eq i, Eq f, LL.ListLike f i) => (f -> Bool) -> Test
-apf msg x = 
--}
+apf :: String -> (forall f i. (Eq i, Eq f, LL.ListLike f i) => (f -> Bool)) -> Test
+apf msg func = 
+    apfi msg newfunc
+    where newfunc x y = func (asTypeOf x (LL.singleton y))
     
 allt = [apfi "empty" prop_empty,
-        apfi "length" prop_length,
-        apfi "to/fromList" prop_tofromlist,
+        apf "length" prop_length,
+        apf "to/fromList" prop_tofromlist,
         apfi "singleton" prop_singleton]
 
 testh = runTestTT (TestList allt)
