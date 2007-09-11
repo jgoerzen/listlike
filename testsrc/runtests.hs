d2065946ce1d6e32f5fb6632fda9653c9e188a01
John Goerzen <jgoerzen@complete.org>
2007-09-11 15:13:23 -0500

    foo

--- testsrc/runtests.hs
+++ testsrc/runtests.hs
@@ -24,12 +24,12 @@ import Data.Monoid
 import TestInfrastructure
 
 allt = 
-       [ta "empty" (mktb (\() -> LL.empty) (\() -> []))
+       [ta "empty" (mktb (\() -> LL.empty) (\() -> [])),
         --tase "empty2" (\_ -> LL.empty) (\_ -> []),
         --tase "singleton" LL.singleton (\x -> [x])
         --ta "singleton" (mktb LL.singleton (\x -> [x]))
         --ta "to/fromList" (LL.toList . LL.fromList) id 
-        -- ta "cons" LL.cons (:)
+        ta "cons" (mktb LL.cons (:))
        ]
 
 testh = runTestTT (TestList allt)
