c1b1ef8f9095707222ede261ed02f5b40f5e4271
Petr Pudlak <petr.mvd@gmail.com>
2013-02-18 12:43:10 +0100

    Inline everything in FoldableLL.

--- src/Data/ListLike/FoldableLL.hs
+++ src/Data/ListLike/FoldableLL.hs
@@ -65,6 +65,7 @@ class FoldableLL full item | full -> item where
     -- This implementation from Data.Foldable
     foldl' f a xs = foldr f' id xs a
         where f' x k z = k $! f z x
+    {-# INLINE foldl' #-}
 
     -- | A variant of 'foldl' with no base case.  Requires at least 1
     -- list element.
@@ -74,6 +75,7 @@ class FoldableLL full item | full -> item where
                     (foldl mf Nothing xs)
            where mf Nothing y = Just y
                  mf (Just x) y = Just (f x y)
+    {-# INLINE foldl1 #-}
     {- | Right-associative fold -}
     foldr :: (item -> b -> b) -> b -> full -> b
 
@@ -82,6 +84,7 @@ class FoldableLL full item | full -> item where
     -- This implementation from Data.Foldable
     foldr' f a xs = foldl f' id xs a
         where f' k x z = k $! f x z
+    {-# INLINE foldr' #-}
 
     -- | Like 'foldr', but with no starting value
     foldr1 :: (item -> item -> item) -> full -> item
@@ -90,50 +93,60 @@ class FoldableLL full item | full -> item where
                     (foldr mf Nothing xs)
            where mf x Nothing = Just x
                  mf x (Just y) = Just (f x y)
+    {-# INLINE foldr1 #-}
 
 
     ------------------------------ Special folds
     {- | Flatten the structure. -}
     concat :: (FoldableLL full' full, Monoid full) => full' -> full
     concat = fold
+    {-# INLINE concat #-}
 
     {- | Map a function over the items and concatenate the results.
          See also 'rigidConcatMap'.-}
     concatMap :: (FoldableLL full item, Monoid full') =>
                  (item -> full') -> full -> full'
     concatMap = foldMap
+    {-# INLINE concatMap #-}
 
     {- | Like 'concatMap', but without the possibility of changing
          the type of the item.  This can have performance benefits
          for some things such as ByteString. -}
     rigidConcatMap :: (Monoid full) => (item -> full) -> full -> full
     rigidConcatMap = concatMap
+    {-# INLINE rigidConcatMap #-}
 
     {- | True if any items satisfy the function -}
     any :: (item -> Bool) -> full -> Bool
     any p = getAny . foldMap (Any . p)
+    {-# INLINE any #-}
 
     {- | True if all items satisfy the function -}
     all :: (item -> Bool) -> full -> Bool
     all p = getAll . foldMap (All . p)
+    {-# INLINE all #-}
 
     {- | The maximum value of the list -}
     maximum :: Ord item => full -> item
     maximum = foldr1 max
+    {-# INLINE maximum #-}
 
     {- | The minimum value of the list -}
     minimum :: Ord item => full -> item
     minimum = foldr1 min
+    {-# INLINE minimum #-}
 
 
     ------------------------------ Searching
     {- | True if the item occurs in the list -}
     elem :: Eq item => item -> full -> Bool
     elem i = any (== i)
+    {-# INLINE elem #-}
 
     {- | True if the item does not occur in the list -}
     notElem :: Eq item => item -> full -> Bool
     notElem i = all (/= i)
+    {-# INLINE notElem #-}
 
     ------------------------------ Conversions
 
@@ -147,18 +160,26 @@ class FoldableLL full item | full -> item where
      @'fold' = 'foldMap' id@ -}
 fold :: (FoldableLL full item, Monoid item) => full -> item
 fold = foldMap id
+{-# INLINE fold #-}
 
 {- | Map each element to a monoid, then combine the results -}
 foldMap :: (FoldableLL full item, Monoid m) => (item -> m) -> full -> m
 foldMap f = foldr (mappend . f) mempty
+{-# INLINE foldMap #-}
 
 instance FoldableLL [a] a where
     foldl = L.foldl
+    {-# INLINE foldl #-}
     foldl1 = L.foldl1
+    {-# INLINE foldl1 #-}
     foldl' = L.foldl'
+    {-# INLINE foldl' #-}
     foldr = L.foldr
+    {-# INLINE foldr #-}
     foldr1 = L.foldr1
+    {-# INLINE foldr1 #-}
     foldr' = F.foldr'
+    {-# INLINE foldr' #-}
 {-
 instance (F.Foldable f) => FoldableLL (f a) a where
     foldl = F.foldl
@@ -177,12 +198,14 @@ traverse_
     :: (Applicative f, FoldableLL full item)
     => (item -> f b) -> full -> f ()
 traverse_ func = foldr ((*>) . func) (pure ())
+{-# INLINE traverse_ #-}
 
 {- | 'for_' is 'traverse_' with its arguments flipped. -}
 for_
     :: (Applicative f, FoldableLL full item)
     => full -> (item -> f b) -> f ()
 for_ = flip traverse_
+{-# INLINE for_ #-}
 
 {- | Evaluate each action in the structure from left to right, and ignore
    the results. -}
@@ -190,12 +213,14 @@ sequenceA_
     :: (Applicative f, FoldableLL full (f item))
     => full -> f ()
 sequenceA_ = traverse_ id
+{-# INLINE sequenceA_ #-}
 
 {- | The sum of a collection of actions, generalizing concat.-}
 asum 
     :: (Alternative f, FoldableLL full (f item))
     => full -> f item
 asum = foldr (<|>) empty
+{-# INLINE asum #-}
 
 
 -- Monadic actions -----------------------------------------------------
@@ -204,22 +229,27 @@ asum = foldr (<|>) empty
 {- | Monadic version of left fold, similar to 'Control.Monad.foldM'. -}
 foldM :: (Monad m, FoldableLL full item) => (a -> item -> m a) -> a -> full -> m a
 foldM f z xs = foldr (\x rest a -> f a x >>= rest) return xs z
+{-# INLINE foldM #-}
 
 {- | A map in monad space, discarding results. -}
 mapM_ :: (Monad m, FoldableLL full item) => (item -> m b) -> full -> m ()
 mapM_ func = foldr ((>>) . func) (return ())
+{-# INLINE mapM_ #-}
 
 {- | A map in monad space, discarding results. -}
 forM_ :: (Monad m, FoldableLL full item) => full -> (item -> m b) -> m ()
 forM_ = flip mapM_
+{-# INLINE forM_ #-}
 
 {- | Evaluate each action, ignoring the results.
    Same as @'mapM_' 'id'@. -}
 sequence_ :: (Monad m, FoldableLL full (m item)) => full -> m ()
 sequence_ = mapM_ id
+{-# INLINE sequence_ #-}
 
 {- | The sum of a collection of actions, generalizing concat.-}
 msum 
     :: (MonadPlus m, FoldableLL full (m item))
     => full -> m item
 msum = foldr mplus mzero
+{-# INLINE msum #-}
